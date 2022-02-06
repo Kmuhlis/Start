2c4990140e7345b5825ab2246a33443ba4739e11
Muhlis <93223751+Kmuhlis@users.noreply.github.com>
2022-02-07 00:03:46 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,24 @@
+package com.company;
+
 public class Main {
     public static void main(String[] args) {
-        // Byte 8 bit
-        byte MAx = 127 , MIn = -128;
-        // short 16 bit
-        short max = 32767 , min = -32768;
-        // integer 32 bit
-        int maxx = 2147483647, minn = -2147483648;
-        // long 64 bit değer sonuna l ekle
-        long Max = 9223372036854775807l, Min = -9223372036854775807l;
-        // float 32 bit değer sonuna "f" koy     max 1.4*10^-45    min 3.4*10^38
-        float MAX = 55.45f, MIN = 25.8f, toplam = MAX + MIN;
-        // double 64 bit max 4.9*10^-324  min 1.8*10^308
-        double maxxx = 8.889, minnn = 4.99546;
-        // char 16 bit 255 e kadar numaralandirilmis ascii kodlarından olusur
-        char vchar = 'b';
-        char v2char = 98;
-        // boolean 2 değeri vardır
-        boolean dogru = true;
-        boolean yanlis = false;
 
-        System.out.println(vchar);
-        System.out.println(v2char);
-        System.out.println(toplam);
+        int sayı1 = 200;
+        int sayı2 = 255;
+        int sayı3 = 379;
+        int sayı4 = 1342;
+        int enBuyuk = 0;
+        if(sayı1>sayı2 ) {
+            enBuyuk = sayı1;
+        }else {
+            enBuyuk = sayı2;
+        }
+        if(enBuyuk<sayı3 ) {
+            enBuyuk = sayı3;
+        }
+        if(enBuyuk<sayı4 ) {
+            enBuyuk = sayı4;
+        }
+        System.out.println(enBuyuk);
     }
 }
